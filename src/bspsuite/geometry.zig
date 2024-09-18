0138a27e383c191c9638a8d0fc86be01994bde47
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-09-18 13:01:59 +0100

    Moved Plane3 to geometry

--- src/bspsuite/geometry.zig
+++ src/bspsuite/geometry.zig
@@ -1,7 +1,9 @@
+pub const Plane3 = @import("geometry/Plane3.zig");
 pub const Winding = @import("geometry/Winding.zig");
 pub const Brush = @import("geometry/Brush.zig");
 
 test {
+    _ = @import("geometry/Plane3.zig");
     _ = @import("geometry/Winding.zig");
     _ = @import("geometry/Brush.zig");
 }
--- src/bspsuite/math.zig
+++ src/bspsuite/math.zig
@@ -1,12 +1,10 @@
 pub usingnamespace @import("math/types.zig");
-pub const Plane3 = @import("math/Plane3.zig");
 pub const Vec3Normal = @import("math/vec3normal.zig").Vec3Normal;
 pub const utils = @import("math/utils.zig");
 pub const constants = @import("math/constants.zig");
 
 test {
     _ = @import("math/types.zig");
-    _ = @import("math/Plane3.zig");
     _ = @import("math/vec3normal.zig");
     _ = @import("math/utils.zig");
     _ = @import("math/constants.zig");
