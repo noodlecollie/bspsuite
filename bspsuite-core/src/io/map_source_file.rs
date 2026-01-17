2110d6ebdb2e915e419961ef98f1fdc9b4e4bd8b
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-01-17 15:50:22 +0000

    Completed code for serialising map source file format v1

--- bspsuite-core/src/io/map_source_file.rs
+++ bspsuite-core/src/io/map_source_file.rs
@@ -18,14 +18,16 @@ where
 
 mod version_1
 {
+	use std::collections::HashMap;
+	use std::io::Write;
+
 	use crate::model::{
 		DPlane3, MapSourceBrush, MapSourceBrushFace, MapSourceEntity, MapSourceFile,
 	};
-	use anyhow::Result;
+	use anyhow::{Context, Result, anyhow};
 	use glam::{DVec2, DVec3};
 	use serde_json::ser::to_writer;
 	use serde_json::{Map, Number, Value};
-	use std::io::Write;
 
 	type JsonObject = Map<String, Value>;
 	type JsonArray = Vec<Value>;
@@ -42,11 +44,7 @@ mod version_1
 	where
 		Writer: Write,
 	{
-		let entities: JsonArray = map
-			.entities
-			.iter()
-			.map(|ent| Value::Object(process_entity(ent)))
-			.collect();
+		let entities: JsonArray = to_json_array(&map.entities, |ent| process_entity(ent))?;
 
 		let mut document: JsonObject = Map::new();
 		document.insert("version".to_string(), VERSION.into());
@@ -56,70 +54,52 @@ mod version_1
 		return Ok(());
 	}
 
-	fn process_entity(entity: &MapSourceEntity) -> JsonObject
+	fn process_entity(entity: &MapSourceEntity) -> Result<JsonObject>
 	{
-		let mut properties: JsonObject = JsonObject::new();
-
-		for (key, value) in entity.keyvalues.iter()
-		{
-			properties.insert(key.clone(), value.clone().into());
-		}
-
-		let brushes: JsonArray = entity
-			.brushes
-			.iter()
-			.map(|brush| Value::Object(process_brush(brush)))
-			.collect();
+		let brushes: JsonArray = to_json_array(&entity.brushes, |brush| process_brush(brush))?;
+		let properties: JsonObject = to_json_object(&entity.keyvalues);
 
 		let mut obj: JsonObject = JsonObject::new();
 		obj.insert("properties".to_string(), properties.into());
 		obj.insert("brushes".to_string(), brushes.into());
 
-		return obj;
+		return Ok(obj);
 	}
 
-	fn process_brush(brush: &MapSourceBrush) -> JsonObject
+	fn process_brush(brush: &MapSourceBrush) -> Result<JsonObject>
 	{
-		let faces: JsonArray = brush
-			.faces
-			.iter()
-			.map(|face| Value::Object(process_face(face)))
-			.collect();
+		let faces: JsonArray = to_json_array(&brush.faces, |face| process_face(face))?;
 
 		let mut obj: JsonObject = JsonObject::new();
 		obj.insert("faces".to_string(), faces.into());
 
-		return obj;
+		return Ok(obj);
 	}
 
-	fn process_face(face: &MapSourceBrushFace) -> JsonObject
+	fn process_face(face: &MapSourceBrushFace) -> Result<JsonObject>
 	{
-		let material_axes = [
-			Value::Array(process_dvec3(&face.material_axes.0)),
-			Value::Array(process_dvec3(&face.material_axes.1)),
-		];
+		let mat_axis_u: JsonArray = process_dvec3(&face.material_axes.0)?;
+		let mat_axis_v: JsonArray = process_dvec3(&face.material_axes.1)?;
+		let mat_offset: JsonArray = process_dvec2(&face.material_offset)?;
+		let mat_scale: JsonArray = process_dvec2(&face.material_scale)?;
+		let plane: JsonArray = process_dplane3(face.plane)?;
+		let material_axes = [Value::Array(mat_axis_u), Value::Array(mat_axis_v)];
 
 		let mut obj: JsonObject = JsonObject::new();
 
-		obj.insert("plane".to_string(), process_dplane3(face.plane).into());
+		obj.insert("plane".to_string(), plane.into());
 		obj.insert(
 			"material_name".to_string(),
 			face.material_name.clone().into(),
 		);
 		obj.insert("material_axes".to_string(), material_axes.to_vec().into());
-		obj.insert(
-			"material_offset".to_string(),
-			process_dvec2(&face.material_offset).into(),
-		);
-		obj.insert(
-			"material_scale".to_string(),
-			process_dvec2(&face.material_scale).into(),
-		);
+		obj.insert("material_offset".to_string(), mat_offset.into());
+		obj.insert("material_scale".to_string(), mat_scale.into());
 
-		return obj;
+		return Ok(obj);
 	}
 
-	fn process_dplane3(plane: DPlane3) -> JsonArray
+	fn process_dplane3(plane: DPlane3) -> Result<JsonArray>
 	{
 		let values: [f64; 4] = [
 			plane.normal.x,
@@ -131,22 +111,49 @@ mod version_1
 		return dvec_slice_to_array(values);
 	}
 
-	fn process_dvec3(vec: &DVec3) -> JsonArray
+	fn process_dvec3(vec: &DVec3) -> Result<JsonArray>
 	{
 		return dvec_slice_to_array(vec.to_array());
 	}
 
-	fn process_dvec2(vec: &DVec2) -> JsonArray
+	fn process_dvec2(vec: &DVec2) -> Result<JsonArray>
 	{
 		return dvec_slice_to_array(vec.to_array());
 	}
 
-	// TODO: Handle errors
-	fn dvec_slice_to_array<const LEN: usize>(contents: [f64; LEN]) -> JsonArray
+	fn dvec_slice_to_array<const LEN: usize>(contents: [f64; LEN]) -> Result<JsonArray>
 	{
-		return contents
-			.into_iter()
-			.map(|num| Value::Number(Number::from_f64(num).unwrap()))
+		return to_json_array(&contents, |num| {
+			Number::from_f64(*num)
+				.ok_or_else(|| anyhow!("Encountered floating point value that was Inf or NaN"))
+		});
+	}
+
+	fn to_json_array<InType, OutType, F>(list: &[InType], callback: F) -> Result<JsonArray>
+	where
+		F: Fn(&InType) -> Result<OutType>,
+		OutType: Into<Value>,
+	{
+		let mut out: JsonArray = JsonArray::with_capacity(list.len());
+
+		for (index, item) in list.iter().enumerate()
+		{
+			let new_item: OutType =
+				callback(item).with_context(|| format!("Failed to serialise item {index}"))?;
+
+			out.push(new_item.into());
+		}
+
+		return Ok(out);
+	}
+
+	fn to_json_object<T>(map: &HashMap<String, T>) -> JsonObject
+	where
+		T: ToString + Into<Value>,
+	{
+		return map
+			.iter()
+			.map(|(key, value)| (key.clone(), Value::String(value.to_string())))
 			.collect();
 	}
 }
