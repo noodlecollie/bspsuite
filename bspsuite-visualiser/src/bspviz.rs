b697945466b976a5add9edc2e5a2cfadea941982
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-05-05 16:28:11 +0100

    Swapped other libs for Raylib

--- bspsuite-visualiser/src/bspviz.rs
+++ bspsuite-visualiser/src/bspviz.rs
@@ -1,198 +1,24 @@
-use std::sync::Arc;
-
 use lazy_static::lazy_static;
 use log::{Level, LevelFilter};
 use paris::formatter::colorize_string;
-use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Window, WindowId};
 
-struct State
-{
-	window: Arc<Window>,
-	device: wgpu::Device,
-	queue: wgpu::Queue,
-	size: winit::dpi::PhysicalSize<u32>,
-	surface: wgpu::Surface<'static>,
-	surface_format: wgpu::TextureFormat,
-}
-
-impl State
-{
-	async fn new(window: Arc<Window>) -> State
-	{
-		let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor::default());
-		let adapter = instance
-			.request_adapter(&wgpu::RequestAdapterOptions::default())
-			.await
-			.unwrap();
-		let (device, queue) = adapter
-			.request_device(&wgpu::DeviceDescriptor::default())
-			.await
-			.unwrap();
-
-		let size = window.inner_size();
-
-		let surface = instance.create_surface(window.clone()).unwrap();
-		let cap = surface.get_capabilities(&adapter);
-		let surface_format = cap.formats[0];
-
-		let state = State {
-			window,
-			device,
-			queue,
-			size,
-			surface,
-			surface_format,
-		};
-
-		// Configure surface for the first time
-		state.configure_surface();
-
-		state
-	}
-
-	fn get_window(&self) -> &Window
-	{
-		&self.window
-	}
-
-	fn configure_surface(&self)
-	{
-		let surface_config = wgpu::SurfaceConfiguration {
-			usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-			format: self.surface_format,
-			// Request compatibility with the sRGB-format texture view we‘re going to create later.
-			view_formats: vec![self.surface_format.add_srgb_suffix()],
-			alpha_mode: wgpu::CompositeAlphaMode::Auto,
-			width: self.size.width,
-			height: self.size.height,
-			desired_maximum_frame_latency: 2,
-			present_mode: wgpu::PresentMode::AutoVsync,
-		};
-		self.surface.configure(&self.device, &surface_config);
-	}
-
-	fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>)
-	{
-		self.size = new_size;
-
-		// reconfigure the surface
-		self.configure_surface();
-	}
-
-	fn render(&mut self)
-	{
-		// Create texture view
-		let surface_texture = self
-			.surface
-			.get_current_texture()
-			.expect("failed to acquire next swapchain texture");
-		let texture_view = surface_texture
-			.texture
-			.create_view(&wgpu::TextureViewDescriptor {
-				// Without add_srgb_suffix() the image we will be working with
-				// might not be "gamma correct".
-				format: Some(self.surface_format.add_srgb_suffix()),
-				..Default::default()
-			});
-
-		// Renders a GREEN screen
-		let mut encoder = self.device.create_command_encoder(&Default::default());
-		// Create the renderpass which will clear the screen.
-		let renderpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-			label: None,
-			color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-				view: &texture_view,
-				depth_slice: None,
-				resolve_target: None,
-				ops: wgpu::Operations {
-					load: wgpu::LoadOp::Clear(wgpu::Color::GREEN),
-					store: wgpu::StoreOp::Store,
-				},
-			})],
-			depth_stencil_attachment: None,
-			timestamp_writes: None,
-			occlusion_query_set: None,
-		});
-
-		// If you wanted to call any drawing commands, they would go here.
-
-		// End the renderpass.
-		drop(renderpass);
-
-		// Submit the command in the queue to execute
-		self.queue.submit([encoder.finish()]);
-		self.window.pre_present_notify();
-		surface_texture.present();
-	}
-}
-
-#[derive(Default)]
-struct App
-{
-	state: Option<State>,
-}
-
-impl ApplicationHandler for App
-{
-	fn resumed(&mut self, event_loop: &ActiveEventLoop)
-	{
-		// Create window object
-		let window = Arc::new(
-			event_loop
-				.create_window(Window::default_attributes())
-				.unwrap(),
-		);
-
-		let state = pollster::block_on(State::new(window.clone()));
-		self.state = Some(state);
-
-		window.request_redraw();
-	}
-
-	fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent)
-	{
-		let state = self.state.as_mut().unwrap();
-		match event
-		{
-			WindowEvent::CloseRequested =>
-			{
-				println!("The close button was pressed; stopping");
-				event_loop.exit();
-			}
-			WindowEvent::RedrawRequested =>
-			{
-				state.render();
-				// Emits a new redraw requested event.
-				state.get_window().request_redraw();
-			}
-			WindowEvent::Resized(size) =>
-			{
-				// Reconfigures the size of the surface. We do not re-render
-				// here as this event is always followed up by redraw request.
-				state.resize(size);
-			}
-			_ => (),
-		}
-	}
-}
+use raylib::prelude as rl;
+use rl::RaylibDraw;
 
 fn main()
 {
 	init_logger();
+	init_raylib_logs();
 
-	let event_loop = EventLoop::new().unwrap();
+	let (mut handle, thread) = raylib::init().size(640, 480).title("Hello, World").build();
 
-	// When the current loop iteration finishes, suspend the thread until
-	// another event arrives. Helps keeping CPU utilization low if nothing
-	// is happening, which is preferred if the application might be idling in
-	// the background.
-	event_loop.set_control_flow(ControlFlow::Wait);
+	while !handle.window_should_close()
+	{
+		let mut d: rl::RaylibDrawHandle = handle.begin_drawing(&thread);
 
-	let mut app = App::default();
-	event_loop.run_app(&mut app).unwrap();
+		d.clear_background(rl::Color::WHITE);
+		d.draw_text("Hello, world!", 12, 12, 20, rl::Color::BLACK);
+	}
 }
 
 fn init_logger()
@@ -228,15 +54,6 @@ fn init_logger()
 
 	let stdout_logger = fern::Dispatch::new()
 		.filter(|md| {
-			// There's quite a lot of wgpu spam that comes through as info logs, so treat
-			// these as debug.
-			if md.level() == Level::Info
-				&& md.target().starts_with("wgpu_hal")
-				&& log::max_level() < Level::Debug
-			{
-				return false;
-			}
-
 			return md.level() == Level::Info
 				|| md.level() == Level::Debug
 				|| md.level() == Level::Trace;
@@ -267,4 +84,21 @@ fn init_logger()
 		.chain(stdout_logger)
 		.apply()
 		.expect("Could not initialise logger");
+}
+
+fn init_raylib_logs()
+{
+	use raylib::consts::TraceLogLevel;
+
+	rl::set_trace_log_callback(|log_level, msg|  {
+		match log_level {
+			TraceLogLevel::LOG_FATAL => panic!("Fatal RayLib error: {msg}"),
+			TraceLogLevel::LOG_ERROR => log::error!(target: "raylib", "[RL] {msg}"),
+			TraceLogLevel::LOG_WARNING => log::warn!(target: "raylib", "[RL] {msg}"),
+			// Treat Raylib info logs as debug, since most of the time they're not relevant to us.
+			TraceLogLevel::LOG_INFO | TraceLogLevel::LOG_DEBUG => log::debug!(target: "raylib", "[RL] {msg}"),
+			TraceLogLevel::LOG_TRACE => log::trace!(target: "raylib", "[RL] {msg}"),
+			_ => panic!("Unexpected log level {:?}", log_level),
+		}
+	}).expect("Could not set Raylib log callback");
 }
