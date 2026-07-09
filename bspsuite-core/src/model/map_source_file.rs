c3358d4af57d8070d8a27a72bb5df43ee466deb9
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-07-09 21:53:09 +0200

    Added test for MapSourceFile v1

--- bspsuite-core/src/model/map_source_file.rs
+++ bspsuite-core/src/model/map_source_file.rs
@@ -6,6 +6,7 @@ use bspextifc::builders::map_source_builder::{Brush, BrushFace, Entity};
 use bspextifc::types::{DPlane3 as ExtPlane, DVec2 as ExtVec2, DVec3 as ExtVec3};
 use glam::{DVec2, DVec3};
 
+#[derive(PartialEq)]
 pub struct MapSourceBrushFace
 {
 	pub global_face_index: usize,
@@ -16,12 +17,14 @@ pub struct MapSourceBrushFace
 	pub material_scale: DVec2,
 }
 
+#[derive(PartialEq)]
 pub struct MapSourceBrush
 {
 	pub global_brush_index: usize,
 	pub faces: Vec<MapSourceBrushFace>,
 }
 
+#[derive(PartialEq)]
 pub struct MapSourceEntity
 {
 	pub global_entity_index: usize,
@@ -29,6 +32,7 @@ pub struct MapSourceEntity
 	pub keyvalues: HashMap<String, String>,
 }
 
+#[derive(PartialEq)]
 pub struct MapSourceFile
 {
 	pub entities: Vec<MapSourceEntity>,
