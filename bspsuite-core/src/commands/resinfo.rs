2ebe0332cbc1e28533e7098dc9c7327f0ac18e6c
Vesper <3692034+noodlecollie@users.noreply.github.com>
2026-07-25 12:16:18 +0100

    Syncing to laptop

--- bspsuite-core/src/commands/resinfo.rs
+++ bspsuite-core/src/commands/resinfo.rs
@@ -3,6 +3,8 @@ use anyhow::anyhow;
 use super::types::{BaseArgs, ResultCode};
 use super::utils::wrap_residual_errors;
 use crate::compiler_error::CompilerError;
+use crate::extensions::ExtensionList;
+use crate::extensions::extension_routines;
 use crate::toolchain::Toolchain;
 
 pub struct ResinfoArgs
@@ -21,8 +23,10 @@ pub fn bspcore_run_resinfo(args: &ResinfoArgs) -> ResultCode
 fn run_resinfo(args: &ResinfoArgs) -> Result<(), CompilerError>
 {
 	let toolchain: Toolchain = Toolchain::new(&args.base.toolchain_root);
+	let extensions: ExtensionList = toolchain.find_extensions();
+	extension_routines::register_all_formats(&extensions);
 
-	// TODO
+	// TODO: We now want to get a stat result for the resource in question.
 	return Err(CompilerError::from_anyhow(
 		crate::CompilerErrorCode::InternalError,
 		anyhow!("TODO: Continue"),
